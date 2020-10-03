bfcd9cbbbb5299f2d71797b36e23c1a24687da59
Artem Artemev <arteitip@gmail.com>
2020-10-03 14:49:09 +0500

    build: added optional purgecss

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,35 +1,40 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-const Dotenv = require('dotenv-webpack');
+const purgecss = require("@fullhuman/postcss-purgecss");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
+  plugins: [
+    new Dotenv({
+      systemvars: true,
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
-        loader: 'postcss-loader',
+        loader: "postcss-loader",
         options: {
-          ident: 'postcss',
-          syntax: 'postcss-scss',
-          plugins: () => [
-            require('postcss-import'),
-            require('tailwindcss')('./tailwind.config.js'),
-            require('autoprefixer'),
-            purgecss({
-              content:  ['./**/*.html', './**/*.ts', './**/*.scss'], 
-              defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-              whitelistPatterns: [/&+/],
-            })
-         
-          ]
-        }
-      }
-    ]
+          ident: "postcss",
+          syntax: "postcss-scss",
+          plugins: () => {
+            const p = [
+              require("postcss-import"),
+              require("tailwindcss")("./tailwind.config.js"),
+              require("autoprefixer"),
+            ];
+            if (process.env.PURGECSS) {
+              p.push(
+                purgecss({
+                  content: ["./**/*.html", "./**/*.ts", "./**/*.scss"],
+                  defaultExtractor: (content) =>
+                    content.match(/[\w-/:]+(?<!:)/g) || [],
+                })
+              );
+            }
+            return p;
+          },
+        },
+      },
+    ],
   },
-  plugins: [
-    new Dotenv({
-      systemvars: true
-    })
-
-  ],
-};
  
+};
